53fdb52ab55f1d549658eb9daad635e35a956043
Edwyn Rodriguez <edwyn3110@hotmail.com>
2018-10-24 11:56:57 -0500

    ruta agregar tutor

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get 'tutors/:idTutor/courses/:idTopic', to: 'courses#show'
   post 'tutors/:idTutor/courses', to: 'courses#create'
   post 'students/:idStudent/courses/:idCourse', to: 'courses#register'
+  post 'tutors', to: 'tutors#create'
 end
