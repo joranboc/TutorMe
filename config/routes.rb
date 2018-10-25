d6034df79dec22b4511ae80f71cbb30ad17d0495
jvelasquezv <jdelrojo1927@gmail.com>
2018-10-25 09:07:41 -0500

    Rutas comentariadas y rutas arregladas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :students
+  #Obtener todos los demas de una materia  
   get 'topics/:idSubject', to: 'topics#index'
+  #Obtener todas las materias
   get 'subjects', to: 'subjects#index'
-  
+
+
   get 'topics/tutors/:idTopic', to: 'topics#tutors'
   get 'tutors/:idTutor/courses/:idTopic', to: 'courses#show'
   post 'tutors/:idTutor/courses', to: 'courses#create'
@@ -13,4 +15,6 @@ Rails.application.routes.draw do
   get 'students/:idStudent', to: 'students#show'
   post 'subject/:idSubject/topics', to: 'topics#create'
   post 'tutors', to: 'tutors#create'
+  
+
 end
